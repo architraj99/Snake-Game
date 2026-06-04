8512790521345103a4926ae6d31c7b02b4ee0e99
architraj99 <rajarchit99@gmail.com>
2026-06-04 11:41:26 +0530

    Made basic layout

---
+++ script.js
@@ -0,0 +1,8 @@
+const score = document.getElementById("score");
+const startBtn = document.getElementById("startBtn");
+const gameBoard = document.getElementById("gameBoard");
+
+startBtn.addEventListener("click", () => {
+
+    startBtn.textContent = "Game Started";
+});
